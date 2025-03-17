c2695da4df336a0ff20c3f66a306245a9bc4416a
Krishthakar <krishthakar198@gmail.com>
2025-03-16 23:12:54 -0400

    Updated portfolio

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -3,7 +3,7 @@ export default function About() {
       <section id="about" className="min-h-screen flex flex-col justify-center items-center text-center px-6 bg-gray-900 text-white">
         <h2 className="text-4xl font-bold text-pink-500 mb-4">About Me</h2>
         <p className="text-lg text-gray-300 max-w-3xl">
-          I'm Isha, a passionate Data Analyst skilled in SQL, Power BI, Python, and Tableau. 
+          I&apos;m Isha, a passionate Data Analyst skilled in SQL, Power BI, Python, and Tableau. 
           I love transforming raw data into meaningful insights to drive business decisions. 
           With hands-on experience in data visualization and analysis, I aim to bring clarity through data.
         </p>
